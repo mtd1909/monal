fab01346634a38e1b9f0910518d1a8dff3f5d09c
Mai Thành Đạt <datmt@timebird.org>
2022-06-13 16:40:20 +0700

    update

--- js/script.js
+++ js/script.js
@@ -1,8 +1,14 @@
 "use strict"
 $(document).ready(function() {
-  $(".owl-carousel").owlCarousel({
+  $(".page_title").owlCarousel({
     items: 1, 
     smartSpeed: 1500,
+  })
+  $(".slide_img").owlCarousel({
+    items: 3, 
+    smartSpeed: 1500,
+    nav: true,
+    margin: 30,
   })
 })
 
@@ -14,3 +20,23 @@ $('.menu_bars').click(function() {
 setTimeout(function() {
   $('#pre-loader').addClass('show');
 }, 0);
+
+var number = 0;
+function demSo() {
+  if(number <32) {
+    number +=1;
+    document.getElementById("count").innerHTML = number + "+";
+  } 
+}
+function findTop(element) {
+  var rec = document.querySelector(element).getBoundingClientRect();
+  return rec.top;
+}
+var isShow = true
+$(document).scroll(function() {
+  var y = $(this).scrollTop();
+  if (y > findTop('#count') && isShow) {
+    isShow = false
+    setInterval(demSo, 100);
+  }
+});
